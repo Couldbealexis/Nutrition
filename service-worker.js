c6958b913fdc994cf6130d7ce009c37765d3e039
Alexis Gutierrez <alexis.gutierrez@alumno.udg.mx>
2019-10-23 17:53:13 -0500

    test sw

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-  '/offline.html',
+  './offline.html',
 ];
 
 self.addEventListener('install', (evt) => {
