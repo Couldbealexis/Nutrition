4b6535471bfcd2776953a35ead9a0abbb2a99698
Alexis Gutierrez <alexis.gutierrez@alumno.udg.mx>
2019-10-23 17:40:30 -0500

    test sw change promise

--- service-worker.js
+++ service-worker.js
@@ -20,16 +20,16 @@ evt.waitUntil(
   })
 );
 
-if (evt.request.mode !== 'navigate') {
-  // Not a page navigation, bail.
-  return;
-}
-evt.respondWith(
-  fetch(evt.request)
-    .catch(() => {
-      return caches.open(CACHE_NAME)
-        .then((cache) => {
-          return cache.match('offline.html');
-        });
-    })
-);
+// if (evt.request.mode !== 'navigate') {
+//   // Not a page navigation, bail.
+//   return;
+// }
+// evt.respondWith(
+//   fetch(evt.request)
+//     .catch(() => {
+//       return caches.open(CACHE_NAME)
+//         .then((cache) => {
+//           return cache.match('offline.html');
+//         });
+//     })
+// );
