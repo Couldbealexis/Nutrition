4a9122f5a50892f2045477fe224fb82f7addd2f4
Alexis Gutierrez <alexis.gutierrez@alumno.udg.mx>
2019-10-23 19:37:32 -0500

    notifications for store

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -254,7 +254,7 @@ md = {
 
     $.notify({
       icon: "add_alert",
-      message: "Welcome to <b>Material Dashboard Pro</b> - a beautiful admin panel for every web developer."
+      message: "Producto agregado al carrito"
 
     }, {
       type: type[color],
