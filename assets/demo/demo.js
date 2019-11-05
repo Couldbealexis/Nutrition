710343be02b3f32e22d29fa1e506aab133a1bb59
Alexis Gutierrez <alexis.gutierrez@alumno.udg.mx>
2019-11-05 09:41:54 -0600

    beta

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -945,109 +945,109 @@ demo = {
 
 
     // Custom Skin & Settings Map
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-      zoom: 13,
-      center: myLatlng,
-      scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-      disableDefaultUI: true, // a way to quickly hide all controls
-      zoomControl: true,
-      styles: [{
-        "featureType": "water",
-        "stylers": [{
-          "saturation": 43
-        }, {
-          "lightness": -11
-        }, {
-          "hue": "#0088ff"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "hue": "#ff0000"
-        }, {
-          "saturation": -100
-        }, {
-          "lightness": 99
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#808080"
-        }, {
-          "lightness": 54
-        }]
-      }, {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#ece2d9"
-        }]
-      }, {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#ccdca1"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#767676"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-          "color": "#ffffff"
-        }]
-      }, {
-        "featureType": "poi",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "visibility": "on"
-        }, {
-          "color": "#b8cb93"
-        }]
-      }, {
-        "featureType": "poi.park",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.sports_complex",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.medical",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.business",
-        "stylers": [{
-          "visibility": "simplified"
-        }]
-      }]
+    // var myLatlng = new google.maps.LatLng(20.680171, -103.331063);
+    // var mapOptions = {
+    //   zoom: 16,
+    //   center: myLatlng,
+    //   scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+    //   disableDefaultUI: true, // a way to quickly hide all controls
+    //   zoomControl: true,
+    //   styles: [{
+    //     "featureType": "water",
+    //     "stylers": [{
+    //       "saturation": 43
+    //     }, {
+    //       "lightness": -11
+    //     }, {
+    //       "hue": "#0088ff"
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "hue": "#ff0000"
+    //     }, {
+    //       "saturation": -100
+    //     }, {
+    //       "lightness": 99
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "geometry.stroke",
+    //     "stylers": [{
+    //       "color": "#808080"
+    //     }, {
+    //       "lightness": 54
+    //     }]
+    //   }, {
+    //     "featureType": "landscape.man_made",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "color": "#ece2d9"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.park",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "color": "#ccdca1"
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "labels.text.fill",
+    //     "stylers": [{
+    //       "color": "#767676"
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "labels.text.stroke",
+    //     "stylers": [{
+    //       "color": "#ffffff"
+    //     }]
+    //   }, {
+    //     "featureType": "poi",
+    //     "stylers": [{
+    //       "visibility": "off"
+    //     }]
+    //   }, {
+    //     "featureType": "landscape.natural",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }, {
+    //       "color": "#b8cb93"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.park",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.sports_complex",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.medical",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.business",
+    //     "stylers": [{
+    //       "visibility": "simplified"
+    //     }]
+    //   }]
 
-    }
+    // }
 
-    var map = new google.maps.Map(document.getElementById("customSkinMap"), mapOptions);
+    // var map = new google.maps.Map(document.getElementById("customSkinMap"), mapOptions);
 
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      title: "Custom Skin & Settings Map!"
-    });
+    // var marker = new google.maps.Marker({
+    //   position: myLatlng,
+    //   title: "Custom Skin & Settings Map!"
+    // });
 
-    marker.setMap(map);
+    // marker.setMap(map);
 
 
 
@@ -1068,6 +1068,114 @@ demo = {
     });
 
     marker.setMap(map);
+
+
+    // // 3 person
+    // // Custom Skin & Settings Map
+    // var myLatlng = new google.maps.LatLng(20.750644, -103.364738);
+    // var mapOptions = {
+    //   zoom: 16,
+    //   center: myLatlng,
+    //   scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+    //   disableDefaultUI: true, // a way to quickly hide all controls
+    //   zoomControl: true,
+    //   styles: [{
+    //     "featureType": "water",
+    //     "stylers": [{
+    //       "saturation": 43
+    //     }, {
+    //       "lightness": -11
+    //     }, {
+    //       "hue": "#0088ff"
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "hue": "#ff0000"
+    //     }, {
+    //       "saturation": -100
+    //     }, {
+    //       "lightness": 99
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "geometry.stroke",
+    //     "stylers": [{
+    //       "color": "#808080"
+    //     }, {
+    //       "lightness": 54
+    //     }]
+    //   }, {
+    //     "featureType": "landscape.man_made",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "color": "#ece2d9"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.park",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "color": "#ccdca1"
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "labels.text.fill",
+    //     "stylers": [{
+    //       "color": "#767676"
+    //     }]
+    //   }, {
+    //     "featureType": "road",
+    //     "elementType": "labels.text.stroke",
+    //     "stylers": [{
+    //       "color": "#ffffff"
+    //     }]
+    //   }, {
+    //     "featureType": "poi",
+    //     "stylers": [{
+    //       "visibility": "off"
+    //     }]
+    //   }, {
+    //     "featureType": "landscape.natural",
+    //     "elementType": "geometry.fill",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }, {
+    //       "color": "#b8cb93"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.park",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.sports_complex",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.medical",
+    //     "stylers": [{
+    //       "visibility": "on"
+    //     }]
+    //   }, {
+    //     "featureType": "poi.business",
+    //     "stylers": [{
+    //       "visibility": "simplified"
+    //     }]
+    //   }]
+
+    // }
+
+    // var map = new google.maps.Map(document.getElementsByClassName("customSkinMap3")[0], mapOptions);
+
+    // var marker = new google.maps.Marker({
+    //   position: myLatlng,
+    //   title: "Custom Skin & Settings Map!"
+    // });
+
+    // marker.setMap(map);
+
 
 
   }
